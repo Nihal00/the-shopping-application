26de50364511d26ccbeb6b540718d7726a9b4428
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-07-17 08:52:08 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import './App.css';
 import Balance from './components/Balance';
 import Budget from './components/Budgets';
 
+const Bal = [
+  {
+    title: "Remaining",
+    Amt: "1020"
+  },
+  {
+    title: "Spent so far",
+    Amt: "1020"
+  }
+]
+
 function App() {
   return (
     <div className="App">
      <h1>Company's Budget Allocation</h1>
      <div className="tabs">
        <Budget />
-       <Balance />
+       {Bal.map((bala) => <Balance {bala.title} />)}
      </div>
     </div>
   );
